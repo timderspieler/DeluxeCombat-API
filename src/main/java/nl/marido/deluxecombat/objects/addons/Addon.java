a685cc4f72ef39832f19c8484039460b9fa10496
timderspieler <timderspieler@gmx.de>
2023-03-11 19:17:27 +0100

    Update for Addon Store

---
+++ src/main/java/nl/marido/deluxecombat/objects/addons/Addon.java
@@ -0,0 +1,53 @@
+package nl.marido.deluxecombat.objects.addons;
+
+import nl.marido.deluxecombat.utils.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Addon {
+
+    private Map<String, Pair<SettingDescription, Boolean>> booleanMap;
+    private Map<String, Pair<SettingDescription, Integer>> integerMap;
+    private Map<String, Pair<SettingDescription, List<String>>> listMap;
+    private Map<String, Pair<SettingDescription, Double>> doubleMap;
+    private Map<String, Pair<SettingDescription, String>> stringMap;
+
+    public Addon() {
+        this.booleanMap = new HashMap<>();
+        this.integerMap = new HashMap<>();
+        this.listMap = new HashMap<>();
+        this.doubleMap = new HashMap<>();
+        this.stringMap = new HashMap<>();
+    }
+
+    public Map<String, Pair<SettingDescription, Boolean>> getBooleanMap() { return booleanMap; }
+    public Map<String, Pair<SettingDescription, Integer>> getIntegerMap() { return integerMap; }
+    public Map<String, Pair<SettingDescription, List<String>>> getListMap() { return listMap; }
+    public Map<String, Pair<SettingDescription, Double>> getDoubleMap() { return doubleMap; }
+    public Map<String, Pair<SettingDescription, String>> getStringMap() { return stringMap; }
+
+    public abstract String getName();
+    public abstract String getInternalName();
+    public abstract String getAuthor();
+    public abstract List<String> getRequiredPlugins();
+    public abstract String getVersion();
+    public abstract String getMaterial();
+    public abstract String getDescription();
+
+    public class SettingDescription {
+        private String name;
+        private String description;
+        private String allowedValues;
+        public SettingDescription(String name, String description, String allowedValues) {
+            this.name = name;
+            this.description = description;
+            this.allowedValues = allowedValues;
+        }
+        public String getName() { return name; }
+        public String getDescription() { return description; }
+        public String getAllowedValues() { return allowedValues; }
+    }
+
+}
