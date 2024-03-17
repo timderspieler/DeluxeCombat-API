59278150fcdd0ed1f812d20aac209e0ae6eb3fad
timderspieler <timderspieler@gmx.de>
2024-03-17 22:09:03 +0100

    PlayerTogglePVPEvent is now toggleable

--- src/main/java/nl/marido/deluxecombat/events/PlayerTogglePVPEvent.java
+++ src/main/java/nl/marido/deluxecombat/events/PlayerTogglePVPEvent.java
@@ -1,16 +1,19 @@
 package nl.marido.deluxecombat.events;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class PlayerTogglePVPEvent extends Event {
+public class PlayerTogglePVPEvent extends Event implements Cancellable {
 
     private static final HandlerList HANDLERS = new HandlerList();
 
     private Player player;
     private boolean pvp_status_before;
     private boolean pvp_status_after;
+    private boolean isCancelled;
+    private String reason;
 
     public PlayerTogglePVPEvent(Player player, boolean pvp_status_before, boolean pvp_status_after) {
         this.player = player;
@@ -21,13 +24,18 @@ public class PlayerTogglePVPEvent extends Event {
     public Player getPlayer() { return player; }
     public boolean getNewPVPStatus() { return pvp_status_after; }
     public boolean getOldPVPStatus() { return pvp_status_before; }
+    public String getReason() { return reason; }
 
     public HandlerList getHandlers() {
         return HANDLERS;
     }
-
+    public boolean isCancelled() { return this.isCancelled; }
+    public void setCancelled(boolean isCancelled) { this.isCancelled = isCancelled; }
     public static HandlerList getHandlerList() {
         return HANDLERS;
     }
-
+    public void cancel(String reason) {
+        this.isCancelled = true;
+        this.reason = reason;
+    }
 }
